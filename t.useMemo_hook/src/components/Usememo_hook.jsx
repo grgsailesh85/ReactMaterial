9cfbf0cb81fa2227a080554e3ae367f6c626acdb
grgsailesh85 <143858404+grgsailesh85@users.noreply.github.com>
2024-03-22 20:04:21 +0545

    updated

--- t.useMemo_hook/src/components/Usememo_hook.jsx
+++ t.useMemo_hook/src/components/Usememo_hook.jsx
@@ -14,3 +14,4 @@ const Usememo_hook = ({ a, b }) => {
 };
 
 export default Usememo_hook;
+//sum is memorized using usememo and the function inside useMemo calculate the sum of a and b. The dependencies [a,b] indicate that the sum should be recalculated whenever a and b changes. If neither a nor b changes the previously memorized value of the sum will be reused.
